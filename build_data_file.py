13d0399868690b7ec7eeea9953b86f21bfa83aa3
ybacoder <ybacoder@gmail.com>
2020-05-02 13:47:27 -0500

    purge invalid data entries

--- build_data_file.py
+++ build_data_file.py
@@ -1,7 +1,15 @@
 import os
 import pandas as pd
 
+def import_data(file_path, cols, index_col="STU_ID", low_memory=False):
+    
+    df = pd.read_csv(file_path, index_col=index_col, usecols=cols, low_memory=low_memory)
+    
+    return df
+
 els_file_path = os.path.join("data", "ELS-2002", "els_02_12_byf3pststu_v1_0.csv")
+nels_file_path = os.path.join("data", "NELS-1988", "nels_88_00_byf4stu_v1_0.csv")
+
 cols = [
     "STU_ID",
     "BYSEX",
@@ -39,7 +47,18 @@ cols = [
     "F1RGPP2"
     ]
 
-df = pd.read_csv(els_file_path, index_col="STU_ID", usecols=cols, low_memory=False)
+
+df = import_data(els_file_path, cols)
 
 if __name__ == "__main__":
-    print(df.head())
+    print(df)
+
+
+df_valid = df >= 0 # remove all invalid entries of data
+df = df[df_valid.all(axis=1)]
+
+df = df[df["BYTVVIGM"] != 99] # remove top_coded BYTVVIGM values
+
+if __name__ == "__main__":
+    print(df)
+
