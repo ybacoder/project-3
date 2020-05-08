45d5ce6af4965154e83e7d61c855dbf8dc1ae3f2
Anne <rendino57@gmail.com>
2020-05-08 18:20:12 -0500

    Make Changes to Form route.

--- app.py
+++ app.py
@@ -1,53 +1,25 @@
+# -*- coding: utf-8 -*-
 import os
 import json
 import pandas as pd
-from fastapi import FastAPI, Request, Depends, BackgroundTasks
+from fastapi import FastAPI, Request, Depends, BackgroundTasks, Form
 from fastapi.staticfiles import StaticFiles
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.encoders import jsonable_encoder
 from fastapi.responses import FileResponse, RedirectResponse, Response
-from pydantic import BaseModel
+from fastapi.templating import Jinja2Templates
+# from database import SessionLocal, engine
+# from pydantic import BaseModel
+# from sqlalchemy.orm import Session
+# import models
 import uvicorn
 from joblib import load
 
-
-class Student(BaseModel):
-    BYSEX: int
-    BYRACE: int
-    BYSTLANG: int
-    BYPARED: int
-    BYINCOME: int
-    BYURBAN: int
-    BYREGION: int
-    BYRISKFC: int
-    BYS34A: int
-    BYS34B: int
-    BYWRKHRS: int
-    BYS42: int
-    BYS43: int
-    BYTVVIGM: int
-    BYS46B: int
-    BYS44C: int
-    BYS20E: int
-    BYS87C: int
-    BYS20D: int
-    BYS23C: int
-    BYS37: int
-    BYS27I: int
-    BYS90D: int
-    BYS38A: int
-    BYS20J: int
-    BYS24C: int
-    BYS24D: int
-    BYS54I: int
-    BYS84D: int
-    BYS84I: int
-    BYS85A: int
-
+file_path = "index.html"
 
 rus_clf = load("rus_clf.joblib")
 
-app = FastAPI()
+app = FastAPI(debug=True)
 
 origins = [
     "http://127.0.0.1:63800",
@@ -62,29 +34,76 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+# models.Base.metadata.create_all(bind=engine)
+
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
-# templates = Jinja2Templates(directory="templates")
+templates = Jinja2Templates(directory="templates")
 
-
-@app.route("/")
-async def home():
+@app.get("/")
+async def home(item_id="templates/index.html"):
     '''
     displays the home page
     '''
-    return FileResponse("index.html")
+    return FileResponse(item_id)
 
 
-@app.route("/student_success")
-async def student_success():
-    '''
-    creates a student and stores it to the database
-    '''
-    return {
-        "code": "success",
-        "message": "student created"
-    }
+@app.api_route("/student_success", methods=["GET", "POST"])
+async def student_success(request: Request, BYSEX: int = Form(default=''),
+    BYRACE: int = Form(default=''),BYSTLANG: int = Form(default=''),BYPARED: int = Form(default=''),
+    BYINCOME: int = Form(default=''),BYURBAN: int = Form(default=''),BYREGION: int = Form(default=''),
+    BYRISKFC: int = Form(default=''),BYS34A: int = Form(default=''),BYS34B: int = Form(default=''),
+    BYWRKHRS: int = Form(default=''),BYS42: int = Form(default=''),BYS43: int = Form(default=''),
+    BYTVVIGM: int = Form(default=''),BYS46B: int = Form(default=''),BYS44C: int = Form(default=''),
+    BYS20E: int = Form(default=''),BYS87C: int = Form(default=''),BYS20D: int = Form(default=''),
+    BYS23C: int = Form(default=''),BYS37: int = Form(default=''),BYS27I: int = Form(default=''),
+    BYS90D: int = Form(default=''),BYS38A: int = Form(default=''),BYS20J: int = Form(default=''),
+    BYS24C: int = Form(default=''),BYS24D: int = Form(default=''),BYS54I: int = Form(default=''),
+    BYS84D: int = Form(default=''),BYS84I: int = Form(default=''),BYS85A: int = Form(default='')
+):
+    if request.method == "GET":
+        return FileResponse("templates/form.html")
+    if request.method == "POST":
 
+        
+        student_list = [BYSEX, BYRACE, BYSTLANG, BYPARED, BYINCOME, BYURBAN, BYREGION, BYRISKFC, BYS34A,
+        BYS34B, BYWRKHRS, BYS42, BYS43, BYTVVIGM, BYS46B, BYS44C, BYS20E, BYS87C, BYS20D, BYS23C, BYS37,
+        BYS27I, BYS90D, BYS38A, BYS20J, BYS24C, BYS24D, BYS54I, BYS84D, BYS84I, BYS85A]
+
+        X = [student_list]
+
+        gpa_range_bin = rus_clf.predict(X)[0]
+
+        gpa_range_dict = {
+            0: "0.00 - 1.50 (Equivalent to D or F average)",
+            1: "1.51 - 2.00 (Equivalent to C average)",
+            2: "2.01 - 3.50 (Equivalent to B average)",
+            3: "3.51 - 4.00 (Equivalent to A average)"
+        }
+
+        gpa_range = gpa_range_dict[gpa_range_bin]
+        
+        probability_of_gpa_range = rus_clf.predict_proba(X)[0][gpa_range_bin]
+
+        grade_range_proba = json.dumps({
+            "gpa_range": gpa_range,
+            "probability_of_gpa_range": probability_of_gpa_range
+        })
+        print(grade_range_proba)
+
+        return templates.TemplateResponse("form.html", {"request": Request, "gpa" : gpa_range, "probability" : probability_of_gpa_range})
+
+
+
+
+# @app.get("/student_success/")
+# async def render_form():
+#     return FileResponse("templates/form.html")
+
+# @app.post("/student_success/")
+# async def submit_form(BYSEX: str = Form(default=''),
+#     BYRACE: str = Form(default='')):
+#     return FileResponse("templates/form.html")
 
 @app.route("/plotly")
 async def plotly():
@@ -95,35 +114,6 @@ async def plotly():
         "code": "success",
         "message": "student created"
     }
-
-
-@app.post("/predict")
-async def predict(student: Student):
-
-    student_list = []
-    for i in student:
-        student_list.append(i[1])
-
-    X = [student_list]
-
-    gpa_range_bin = rus_clf.predict(X)[0]
-
-    gpa_range_dict = {
-        0: "0.00 - 1.50 (Equivalent to D or F average)",
-        1: "1.51 - 2.00 (Equivalent to C average)",
-        2: "2.01 - 3.50 (Equivalent to B average)",
-        3: "3.51 - 4.00 (Equivalent to A average)"
-    }
-    gpa_range = gpa_range_dict[gpa_range_bin]
-    
-    proability_of_gpa_range = rus_clf.predict_proba(X)[0][gpa_range_bin]
-
-    grade_range_proba = json.dumps({
-        "gpa_range": gpa_range,
-        "proability_of_gpa_range": proability_of_gpa_range
-    })
-
-    return Response(content=grade_range_proba, media_type="application/json")
 
 
 @app.get("/get_student_data")
